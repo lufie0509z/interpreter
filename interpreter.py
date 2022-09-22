cdc02fc1e526f50bf85e78036ffdba9650d6e5e2
lufie <lufie@192.168.18.2>
2022-09-22 18:01:53 +0800

    support builtin funcs

--- interpreter.py
+++ interpreter.py
@@ -1,10 +1,19 @@
+from ast import arg
 import dis
+import builtins
+from unittest import result
 
 class Interpreter:
-    def __init__(self, source):
+    def __init__(self, source, local_vals = None, dump_code = False, trace_stack = False):
         self._code = compile(source, filename = '', mode = 'exec')
         self._locals = {}
         self._stack = []
+        self._dump_code = dump_code
+        self._trace_stack = trace_stack
+        self._builtins = {x: getattr(builtins, x) for x in dir(builtins) if not x.startswith('__')}
+        if local_vals:
+            for k, v in local_vals.items():
+                self.set_local(k, v)
 
     def get_local(self, name):
         return self._locals[name]
@@ -24,6 +33,13 @@ class Interpreter:
     def stack_pop(self):
         return self._stack.pop(-1)
 
+    def stack_popn(self, count):
+        if count:
+            result = self._stack[-count:]
+            self._stack = self._stack[:-count]
+            return result
+        return []
+
     def dump_code(self):
         print("====dis code==")
         print('co_names:', self._code.co_names)
@@ -35,18 +51,23 @@ class Interpreter:
     def dump_stack(self, instruction):
          print(f'Stack after {instruction.opname}({instruction.offset}): {self._stack}')
 
-    def exec(self, dump_code = False, trace_stack = False):
-        if dump_code:
+    def exec(self):
+        if self._dump_code:
             self.dump_code()
         for instruction in dis.get_instructions(self._code):
             fn = getattr(self, 'exec_' + instruction.opname)
             fn(instruction.arg)
-            if trace_stack:
-                self.trace_stack()
+            if self._trace_stack:
+                self.dump_stack(instruction)
 
     def exec_LOAD_NAME(self, namei):
         name = self.get_name(namei)
-        value = self.get_local(name)
+        if name in self._locals:
+            value = self.get_local(name)
+        elif name in self._builtins:
+            value = self._builtins[name]
+        else:
+            raise NameError(name)
         self.stack_push(value)
 
     def exec_LOAD_CONST(self, consti):
@@ -65,3 +86,8 @@ class Interpreter:
     def exec_RETURN_VALUE(self, _):
         pass
 
+    def exec_CALL_FUNCTION(self, argc):
+        argc = self.stack_popn(argc)
+        func = self._stack.pop(-1)
+        result = func(*argc)
+        self.stack_push(result)
--- test_interpreter.py
+++ test_interpreter.py
@@ -1,11 +1,22 @@
 import unittest 
 from .interpreter import Interpreter
+import interpreter
 
 class InterpreterTest(unittest.TestCase):
+    def exec_interpreter(self, source, local_vals = None, dump_code = False, trace_stack = False):
+        interpreter = Interpreter(source, local_vals = local_vals, dump_code = dump_code, trace_stack = trace_stack)
+        interpreter.exec()
+        return interpreter
+
+
     def test_add(self):
         source = "a = b + 1"
-        interpreter = Interpreter(source)
-        interpreter.set_local('b', 2)
-        interpreter.exec()
-        self.assertEqual(3, interpreter.get_local('a'))
+        interpreter = self.exec_interpreter(source, {'b': 1}, False, False)
+        self.assertEqual(2, interpreter.get_local('a'))
 
+
+    def test_call_func(self):
+        source = "n = divmod(a, 2)"
+        interpreter = self.exec_interpreter(source, {'a': 11})
+        self.assertEqual((5, 1), interpreter.get_local('n'))
+   
